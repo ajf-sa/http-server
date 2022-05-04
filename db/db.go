c8c22ed9521f208ae221be1e15d0aa36f68be421
alfuhigi <abajall@gmail.com>
2022-05-04 22:03:29 +0300

    create new service

--- db/db.go
+++ db/db.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"database/sql"
-	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -16,14 +15,8 @@ func New(driverName, dataSourceName string) (*DB, error) {
 	if err != nil {
 		return nil, err
 	}
+	r := &DB{db}
+	r.CreateTableUser()
 
-	sqlStmt := `
-	create table if not exists users (id integer not null primary key AUTOINCREMENT, name text);
-	`
-	_, err = db.Exec(sqlStmt)
-	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return nil, err
-	}
-	return &DB{db}, nil
+	return r, nil
 }
